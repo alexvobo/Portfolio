22ef8ae061c5fa39378abff32f6ed814efa21120
Alexsandr Vobornov <vobos2@gmail.com>
2020-07-30 19:21:18 -0400

    Removed resume, added nice gallery for Flickr, renovated portfolio section

--- js/feedCarousel.js
+++ js/feedCarousel.js
@@ -1,93 +1,29 @@
 import config from '../config.js';
 
 const key1 = config.key1;
-const key2 = config.key2;
-const url = "https://www.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=" + key1 + "&user_id=186575794%40N06&format=json&nojsoncallback=1";
 
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-            //console.log(data);
-            let n = data;
-            var _s = n.photos.photo;
-            let photoURLs = [];
-            for (var z = 0; z < n.photos.photo.length; z++) {
-                var CurrentPhotoUrl = 'https://farm' + _s[z]['farm'] + '.staticflickr.com/' + _s[z]['server'] + '/' + _s[z]['id'] + '_' + _s[z]['secret'] + '_b.jpg';
-                photoURLs.push(CurrentPhotoUrl);
-                //console.log(CurrentPhotoUrl);
-            }
-            photoURLs.forEach(addImg);
+$(function () {
+    $.ajax({
+        url: "https://api.flickr.com/services/rest/",
+        data: {
+            method: "flickr.people.getPublicPhotos",
+            api_key: key1,
+            user_id:"186575794@N06",
+            format: "json",
+            nojsoncallback: 1
+        },
+        success: function (response) {
+            // console.log(response);
+            $.each(response.photos.photo, function (index, value) {
+                console.log(value);
+                var url = 'https://farm' + value.farm + '.staticflickr.com/' + value.server + '/' + value.id + '_' + value.secret + '_b.jpg';
+                var a = $('<a>').attr({href: url});
+                var img = $('<img>').attr({src: url});
 
-            /*       let img = document.createElement('img');
-                   img.src = photoURLs[0];
-                   img.className = "flickrFeed";
-
-                   let imgDiv = document.createElement("div");
-                   imgDiv.className = "item";
-                   imgDiv.append(img);
-                   document.getElementById("feedSrc").appendChild(imgDiv);*/
+                a.append(img);
+                $("#flickrGallery").append(a);
+            });
+            $('#flickrGallery').justifiedGallery({rowHeight:300,margins:10});
         }
-    );
-
-/*
-function findBiggest(URL) {
-    let img = document.createElement('img');
-    img.src = URL;
-
-    var onLoad;
-    img.on('load', onLoad = function () {
-        done(img.width, img.height);
     });
-
-
-    var isDone = false;
-
-    function done() {
-        if (isDone) {
-            return;
-        }
-        isDone = true;
-
-        clearInterval(wait);
-        img.off('load', onLoad);
-
-        callback.apply(this, arguments);
-    }
-
-    if (img.width > imgDims.width) {
-        imgDims['width'] = img.width;
-        console.log("width: " + img.width)
-    }
-    if (img.height > imgDims.height) {
-        imgDims['height'] = img.height;
-        console.log("height: " + img.height)
-    }
-}
-*/
-
-function addImg(URL, index) {
-
-    let img = document.createElement('img');
-    img.src = URL;
-    img.className = "photo img-fluid";
-    let imgDiv = document.createElement("div");
-    imgDiv.className = "carousel-item";
-
-    if (index === 0) {
-        imgDiv.className = "carousel-item active";
-    }
-    imgDiv.append(img);
-
-    document.getElementById("feedSrc").appendChild(imgDiv);
-}
-
-window.onload = function () {
-
-
-};
-
-
-
-
-
-
+});
